2d02172b7810acfa11b66dac890c4488499d5cbf
Flamyon <franrodriguezcarretero@gmail.com>
2022-10-29 16:50:58 +0200

    tupla con los datos del csv

--- src/stockx.py
+++ src/stockx.py
@@ -1,22 +1,23 @@
-from csv import reader
 from datetime import date, datetime
+import csv
 
 
 def extraer_datos():
-    with open('C:/Users/franr/OneDrive/Escritorio/informaticaus\FP\practicas\proyecto-de-laboratorio-Flamyon-main\proyecto-de-laboratorio-Flamyon-main\src\test_stockx.py"dataset1.csv') as f:
-        reader = csv.reader(f)
+    with open('C:/Users/franr/OneDrive/Escritorio/informatica_us/FP/practicas/proyecto-de-laboratorio-Flamyon-main/proyecto-de-laboratorio-Flamyon-main/data/dataset1.csv') as f:
+        lector = csv.reader(f)
         next(f)
         datos = []
-        for columna in reader:
-            fecha_pedido = date(columna[0])
+        for columna in lector:
+            fecha_pedido = columna[0]
             marca = columna[1]
             nombre = columna[2]
             precio_reventa = columna[3]
             precio_venta = columna[4]
-            fecha_lanzamiento = date(columna[5])
+            fecha_lanzamiento = columna[5]
             talla = int(columna[6])
             region_compra = columna[7]
             precio_reventa_menor = bool(columna[8])
             tupla = (fecha_pedido, marca, nombre, precio_reventa, precio_venta,
                      fecha_lanzamiento, talla, region_compra, precio_reventa_menor)
             datos.append(tupla)
+    return datos
--- src/test_stockx.py
+++ src/test_stockx.py
@@ -1,5 +1,5 @@
 from stockx import *
 
 if __name__ == "__main__":
-    extraer_datos()
-    print(datos[1: 2])
+    datos = [extraer_datos()]
+    print(datos)
