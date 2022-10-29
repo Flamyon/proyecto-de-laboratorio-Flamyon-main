018e3acc670da90833f2484b728ca20227d7f46e
Flamyon <franrodriguezcarretero@gmail.com>
2022-10-29 13:41:51 +0200

    with open para crear tupla

--- src/test_stockx.py
+++ src/test_stockx.py
@@ -1,4 +1,23 @@
-#este documento es solo de prueba, todavia no hay código en este proyecto
+# este documento es solo de prueba, todavia no hay código en este proyecto
+from csv import reader
+from datetime import date, datetime
+
+
 def extraer_datos():
-    with dataset1.csv open as f:
-    
+    with open('dataset1.csv', encoding='utf-8') as f:
+        reader = reader.csv(f)
+        next(f)
+        datos = []
+        for columna in reader:
+            fecha_pedido = date(columna[0])
+            marca = columna[1]
+            nombre = columna[2]
+            precio_reventa = columna[3]
+            precio_venta = columna[4]
+            fecha_lanzamiento = date(columna[5])
+            talla = int(columna[6])
+            region_compra = columna[7]
+            precio_reventa_menor = bool(columna[8])
+            tupla = (fecha_pedido, marca, nombre, precio_reventa, precio_venta,
+                     fecha_lanzamiento, talla, region_compra, precio_reventa_menor)
+            datos.append(tupla)
