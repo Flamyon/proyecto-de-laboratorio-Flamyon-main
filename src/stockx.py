fdbdf39069a5ea768a2feec7bcf636dab4d23dc8
Flamyon <franrodriguezcarretero@gmail.com>
2022-10-30 11:10:55 +0100

    Read.me actualizado, inicio entrega2 (tallas distintas)

---
+++ src/stockx.py
@@ -0,0 +1,35 @@
+from datetime import date, datetime
+import csv
+from collections import namedtuple
+from sqlite3 import DateFromTicks
+
+Stock = namedtuple(
+    'Stock', 'FechaPedido,Marca,Nombre,PrecioReventa,PrecioVenta,FechaLanzamiento,Talla,Region,PrecioReventaMenor')
+
+
+def extraer_datos(fichero):
+    with open(fichero, encoding='utf-8') as f:
+        lector = csv.reader(f, delimiter=',')
+        next(lector)
+        datos = list()
+        for columna in lector:
+            FechaPedido = datetime.strptime(columna[0], '%m/%d/%Y').date()
+            Marca = columna[1]
+            Nombre = columna[2]
+            PrecioReventa = float(columna[3])
+            PrecioVenta = float(columna[4])
+            FechaLanzamiento = datetime.strptime(columna[5], '%m/%d/%Y').date()
+            Talla = int(columna[6])
+            Region = columna[7]
+            PrecioReventaMenor = columna[8] == 'TRUE'
+            tupla = Stock(FechaPedido, Marca, Nombre, PrecioReventa, PrecioVenta,
+                          FechaLanzamiento, Talla, Region, PrecioReventaMenor)
+            datos.append(tupla)
+        return datos
+
+
+def tallas_distintas(datos):
+    tallas = set()
+    for i in datos:
+        tallas.add(i.Talla)
+    return len(tallas)
--- src/test_stockx.py
+++ src/test_stockx.py
@@ -1,1 +1,22 @@
-#este documento es solo de prueba, todavia no hay código en este proyecto
+from tkinter import S
+from stockx import *
+
+
+def test_extraer_datos():
+    print('Numero total de registros: ', len(datos), '\n')
+    print('----------------------------------------------------------------------------')
+    print('\nLos datos de los 3 primeros registros leídos: ', datos[:3], '\n')
+    print('----------------------------------------------------------------------------')
+    print('\nY los 3 últimos registros leídos de venta fueron',
+          datos[-3:], '\n')
+    print('----------------------------------------------------------------------------')
+
+
+def test_tallas_distintas():
+    print('\nNumero total de tallas distintas: ', tallas_distintas(datos), '\n')
+    print('----------------------------------------------------------------------------')
+
+
+datos = extraer_datos('data/dataset1.csv')
+test_extraer_datos()
+test_tallas_distintas()
